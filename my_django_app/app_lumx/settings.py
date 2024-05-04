b9c4b56842447e10c17815ce34cf30a9de2ff420
devmatheusoliveira <matheusoliveira.workmso@gmail.com>
2024-05-04 12:14:25 -0300

    conexão com postgres

--- my_django_app/app_lumx/settings.py
+++ my_django_app/app_lumx/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'app_lumx.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'iJgPNKp0ot4f',
+        'HOST': 'ep-noisy-block-a40xhajs-pooler.us-east-1.aws.neon.tech',
+        'PORT': '5432',
     }
 }
 
