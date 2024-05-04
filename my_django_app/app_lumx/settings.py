b6b2d9f7cf40dc5e8ac26047b7cd60bf512e7a9b
larii4272 <larissa.montefusco7@gmail.com>
2024-05-05 00:37:44 +0100

    putting sqlite

--- my_django_app/app_lumx/settings.py
+++ my_django_app/app_lumx/settings.py
@@ -77,14 +77,14 @@ AUTH_USER_MODEL = 'app_lumxs.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'verceldb',
@@ -93,7 +93,7 @@ DATABASES = {
         'HOST': 'ep-noisy-block-a40xhajs-pooler.us-east-1.aws.neon.tech',
         'PORT': '5432',
     }
-}
+}'''
 
 
 # Password validation
