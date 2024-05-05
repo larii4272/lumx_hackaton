065ee811d91ce4e8bad71984cfd92dfd4edab4c9
larii4272 <larissa.montefusco7@gmail.com>
2024-05-05 14:06:24 +0100

    wallet created a transaction object

--- backend_lumx/wallet.py
+++ backend_lumx/wallet.py
@@ -1,6 +1,7 @@
 
 import requests
 import json
+import transaction
 class Wallet():
 
     def __init__(self, project) -> None:
@@ -11,10 +12,48 @@ class Wallet():
         url = "https://protocol-sandbox.lumx.io/v2/wallets"
         headers = {"Authorization": f"Bearer {self.project.apiKey}"}
         response = requests.request("POST", url, headers=headers)
-        print(response.text)
         response_dict = json.loads(response.text)
         self.walletAddress = response_dict['address']
         self.walletId = response_dict['id']
+        return response_dict
+    
+    def invoke_custom_transaction(self, wallet, outsideContractAddress, functionSignature, argumentsValues, messageValue):
+        url = "https://protocol-sandbox.lumx.io/v2/transactions/custom"
 
-        print(response_dict)
+        payload = {
+            "walletId": f"{wallet.walletId}",
+            "contractAddress": f"{outsideContractAddress}", #The contract is from outside
+            "operations": [
+                {
+                    "functionSignature": f"{functionSignature}",
+                    "argumentsValues": [f"{argumentsValues}"],
+                    "messageValue": messageValue
+                }
+            ]
+        }
+        headers = {
+            "Authorization": f"Bearer {self.project.apiKey}",
+            "Content-Type": "application/json"
+        }
+        response = requests.request("POST", url, json=payload, headers=headers)
+        response_dict = json.loads(response.text)
+        print(f"\ninvoke_custom_transaction response text {json.dumps(response_dict, indent=len(response_dict))}")
+        my_transaction = transaction.Transaction(self, response_dict["id"])
+        return my_transaction
 
+    #***********************READ METHODS***********************:
+    def read_wallet(self):
+        url = f"https://protocol-sandbox.lumx.io/v2/wallets/{self.walletId}"
+        headers = {"Authorization": f"Bearer {self.project.apiKey}"}
+        response = requests.request("GET", url, headers=headers)
+        print(response.text)
+        response_dict = json.loads(response.text)
+        return response_dict
+    
+    def read_all_transactions(self):
+        url = "https://protocol-sandbox.lumx.io/v2/transactions"
+        headers = {"Authorization": f"Bearer {self.apiKey}"}
+        response = requests.request("GET", url, headers=headers)
+        print(response.text)
+        response_dict = json.loads(response.text)
+        return response_dict
